762ae5f0097e3f2d1f095706ed4b186a8e382e8c
Darren <darrenzhang200027@gmail.com>
2021-06-01 13:46:39 -0400

    added isvalidsudoku soln

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -2,12 +2,49 @@ from typing import List
 
 from test_framework import generic_test
 
-
+'''
+insert into position, check if valid, recursively call, then remove
+'''
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
-    # TODO - you fill in here.
-    return True
+    n = len(partial_assignment)
+    for r in range(n):
+        s = set()
+        for c in range(n):
+            if partial_assignment[r][c] != 0:
+                s.add(partial_assignment[r][c])
+        l = list(filter(lambda x: x != 0, partial_assignment[r]))
+        if len(s) != len(l):
+            return False
 
+    for c in range(n):
+        s = set()
+        for r in range(n):
+            if partial_assignment[r][c] != 0:
+                s.add(partial_assignment[r][c])
+        arr = []
+        for i in range(n):
+            if partial_assignment[i][c] != 0:
+                arr.append(partial_assignment[i][c])
+        if len(s) != len(arr):
+            return False
+
+    def isValidGrid(mat, startR, startC):
+        s = set()
+        l = []
+        for i in range(startR, startR + 3):
+            for j in range(startC, startC + 3):
+                if partial_assignment[i][j] != 0:
+                    s.add(partial_assignment[i][j])
+                    l.append(partial_assignment[i][j])
+        return len(s) == len(l)
+
+    for startR in [0, 3, 6]:
+        for startC in [0, 3, 6]:
+            if not isValidGrid(partial_assignment, startR, startC):
+                return False
+
+    return True
 
 if __name__ == '__main__':
     exit(
