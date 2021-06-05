c9c0eee531be6d176abcf4cb0dd7fccc0a789cc4
Darren <darrenzhang200027@gmail.com>
2021-06-05 18:18:45 -0400

    added remove and erplace

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -6,8 +6,33 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    removeBs(size, s)
+    replaceAs(size, s)
+    return s
+
+def removeBs(size, s):
+    slow = fast = 0
+    while fast < size:
+        if s[fast] != 'b':
+            s[slow] = s[fast]
+            slow += 1
+        fast += 1
+    for i in range(slow, size):
+        s[i] = None
+    
+def replaceAs(size, s):
+    if None not in s: # no 'a's
+        return 
+    lastElIdx = s.index(None) - 1 
+    indexCopyTo = size - 1
+    for i in range(lastElIdx, -1, -1):
+        if s[i] == 'a':
+            s[indexCopyTo - 1: indexCopyTo + 1] = ['d', 'd']
+            indexCopyTo -= 2
+        else:
+            s[indexCopyTo] = s[i]
+            indexCopyTo -= 1
+            
 
 
 @enable_executor_hook
