2908b38fc38fce977afd5b3d3c6b69e5f9d6380c
Darren <darrenzhang200027@gmail.com>
2021-06-05 21:25:59 -0400

    added reverse words

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -7,9 +7,21 @@ from test_framework.test_utils import enable_executor_hook
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
 def reverse_words(s):
-    # TODO - you fill in here.
-    return
+    s.reverse()
+    start, end = 0, 0
+    while True:
+        try:
+            end = s.index(" ", start, len(s))
+        except ValueError:
+            break
+        reverseWord(s, start, end)
+        start = end + 1
+    reverseWord(s, start, len(s))
+    return s
 
+def reverseWord(s, start, end):
+    for i in range((end - start) // 2):
+        s[start + i], s[end - 1 - i] = s[end - 1 - i], s[start + i]
 
 @enable_executor_hook
 def reverse_words_wrapper(executor, s):
