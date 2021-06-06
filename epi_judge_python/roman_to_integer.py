c83c13c1282133116d1a9de6ded86f7af72a3146
Darren <darrenzhang200027@gmail.com>
2021-06-05 23:33:42 -0400

    roman to integer

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def roman_to_integer(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    exceptions = {'IX': 9, 'IV': 4, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+    total = 0
+    toContinue = False
+    for i, c in enumerate(s):
+        if toContinue:
+            toContinue = False
+            continue
+        if i + 1 < len(s) and s[i:i+2] in exceptions:
+            total += exceptions[s[i:i+2]]
+            toContinue = True
+        else:
+            total += values[c]
+    return total
+
 
 
 if __name__ == '__main__':
