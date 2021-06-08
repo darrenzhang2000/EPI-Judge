d3acf28dc1ff5ec8ac20c06c4d071c3ac3bf1ec2
Darren <darrenzhang200027@gmail.com>
2021-06-08 19:14:22 -0400

    added is list palindromic

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -3,8 +3,33 @@ from test_framework import generic_test
 
 
 def is_linked_list_a_palindrome(L: ListNode) -> bool:
-    # TODO - you fill in here.
+    if not L:
+        return True
+    steps = 0
+    slow = fast = L
+    while fast and fast.next:
+        steps += 1
+        slow = slow.next
+        fast = fast.next.next
+
+    # reverse second half of linked list
+    prev = slow
+    cur = slow.next
+    while cur:
+        temp = cur.next
+        cur.next = prev
+        prev = cur
+        cur = temp
+    h2 = prev
+    h1 = L
+    for _ in range(steps):
+        if h1.data != h2.data:
+            return False
+        h1 = h1.next
+        h2 = h2.next
     return True
+
+
 
 
 if __name__ == '__main__':
