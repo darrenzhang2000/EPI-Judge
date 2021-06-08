fec1083cb6226bda54b2de90344979315c66fabe
Darren <darrenzhang200027@gmail.com>
2021-06-07 22:00:35 -0400

    merge linked lists

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -4,10 +4,23 @@ from list_node import ListNode
 from test_framework import generic_test
 
 
-def merge_two_sorted_lists(L1: Optional[ListNode],
-                           L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+def merge_two_sorted_lists(l1: Optional[ListNode],
+                           l2: Optional[ListNode]) -> Optional[ListNode]:
+    newHead = ListNode()
+    cur = newHead
+    n1, n2 = l1, l2
+    while n1 or n2:
+        v1 = n1.data if n1 else float('inf')
+        v2 = n2.data if n2 else float('inf')
+        if v1 < v2:
+            cur.next = n1 
+            n1 = n1.next
+        else:
+            cur.next = n2
+            n2 = n2.next
+        cur = cur.next
+    return newHead.next
+
 
 
 if __name__ == '__main__':
