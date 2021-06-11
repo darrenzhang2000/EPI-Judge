968de5151ecc62ed169c43d9f5e1a842224e90ea
Darren <darrenzhang200027@gmail.com>
2021-06-11 00:31:20 -0400

    added tree level order

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -2,11 +2,28 @@ from typing import List
 
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
-
+from collections import deque
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    if not tree:
+        return []
+    res = []
+    q = deque([tree])
+    while q:
+        curLevelRes = []
+        nextQ = deque()
+        while q:
+            node = q.popleft()
+            curLevelRes.append(node.data)
+            if node.left: 
+                nextQ.append(node.left)
+            if node.right:
+                nextQ.append(node.right)
+        res.append(curLevelRes)
+        q = nextQ
+    return res
+
+    
 
 
 if __name__ == '__main__':
