759d54f16f9f8c439bd108941c1f15a158146301
Darren <darrenzhang200027@gmail.com>
2021-06-10 22:21:30 -0400

    added directory path normalization

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -2,8 +2,39 @@ from test_framework import generic_test
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    if not path:
+        return
+    '''
+    absolute path
+    /usr/lib/../bin/./gcc
+    stack = [usr, bin, gcc]
+    - .. -> pop if stack isn't empty
+    - .. -> if top equals ".", continue
+    - . -> don't do anything -> continue
+    - if empty -> continue
+
+    relative path
+    ./../../hi/../
+    stack = [.., ]
+    - .. -> if stack is empty, append it
+    - .. -> if top of stack has .., append.
+    - .. -> else, pop from stack
+    '''
+    isAbsolutePath = path[0] == "/"
+    stack = []
+    tokens = filter(lambda token: not (token == "" or token == "."), path.split("/"))
+    if isAbsolutePath:
+        stack.append("/")
+    for token in tokens:
+        if token == "..":
+            if not stack or stack[-1] == "..":
+                stack.append("..")
+            else:
+                stack.pop()
+        else:
+            stack.append(token)
+    res = "/".join(stack)
+    return res[res.startswith('//'):]
 
 
 if __name__ == '__main__':
