0837359cfaf50dc392026fd0709301b99a41a8ca
Darren <darrenzhang200027@gmail.com>
2021-06-13 16:49:59 -0400

    added lowest common ancesstor

--- epi_judge_python/lowest_common_ancestor.py
+++ epi_judge_python/lowest_common_ancestor.py
@@ -6,32 +6,56 @@ from test_framework import generic_test
 from test_framework.binary_tree_utils import must_find_node, strip_parent_link
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
-
-def findPath(tree, node, res, path):
-    if not tree:
-        return
-    newPath = path[:] + [tree]
-    if tree is node:
-        res.path = newPath[:]
-    findPath(tree.left, node, res, newPath)
-    findPath(tree.right, node, res, newPath)
-
-class Result:
-    def __init__(self):
-        self._found = False
-        self.path = []
+from collections import namedtuple
 
 def lca(tree: BinaryTreeNode, node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-        leftR = Result()
-        findPath(tree, node0, leftR, [])
-        rightR = Result()
-        findPath(tree, node1, rightR, [])
-        path0 = leftR.path
-        path1 = rightR.path
-        for i in range(min(len(path0), len(path1)) - 1, -1, -1):
-            if path0[i] is path1[i]:
-                return path0[i]
+        Status = namedtuple("Status", ("numTargetNodes" ,"ancestor"))
+        def helper(tree, node0, node1):
+            if not tree:
+                return Status(0, None)
+
+            leftStatus = helper(tree.left, node0, node1)
+            if leftStatus.numTargetNodes == 2:
+                return leftStatus
+
+            rightStatus = helper(tree.right, node0, node1)
+            if rightStatus.numTargetNodes == 2:
+                return rightStatus
+            
+            numTargetNodes = leftStatus.numTargetNodes + rightStatus.numTargetNodes + [node0, node1].count(tree)
+
+            return Status(numTargetNodes, tree if numTargetNodes == 2 else None)
+        return helper(tree, node0, node1).ancestor
+
+
+
+# def findPath(tree, node, res, path):
+#     if not tree:
+#         return
+#     path.append(tree)
+#     if tree is node:
+#         res.path = path[:]
+#     findPath(tree.left, node, res, path)
+#     findPath(tree.right, node, res, path)
+#     path.pop()
+
+# class Result:
+#     def __init__(self):
+#         self._found = False
+#         self.path = []
+
+# def lca(tree: BinaryTreeNode, node0: BinaryTreeNode,
+#         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+#         leftR = Result()
+#         findPath(tree, node0, leftR, [])
+#         rightR = Result()
+#         findPath(tree, node1, rightR, [])
+#         path0 = leftR.path
+#         path1 = rightR.path
+#         for i in range(min(len(path0), len(path1)) - 1, -1, -1):
+#             if path0[i] is path1[i]:
+#                 return path0[i]
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, key1, key2):
