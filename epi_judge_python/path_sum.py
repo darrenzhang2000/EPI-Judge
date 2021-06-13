4705fa38cc5c1634dd8347ac3a38efe67028fd10
Darren <darrenzhang200027@gmail.com>
2021-06-13 15:17:59 -0400

    finished path sum

--- epi_judge_python/path_sum.py
+++ epi_judge_python/path_sum.py
@@ -1,11 +1,23 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
-
 def has_path_sum(tree: BinaryTreeNode, remaining_weight: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    if not tree:
+        return False
 
+    remaining_weight -= tree.data
+
+    # leaf
+    if not tree.left and not tree.right:
+        return remaining_weight == 0
+
+    # non-leaf
+    if has_path_sum(tree.left, remaining_weight):
+        return True
+    if has_path_sum(tree.right, remaining_weight):
+        return True
+
+    return False
 
 if __name__ == '__main__':
     exit(
