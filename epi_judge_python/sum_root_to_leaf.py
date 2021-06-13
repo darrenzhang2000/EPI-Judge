a1b8dc4c4b1eaa6838e7e971843117effa51a005
Darren <darrenzhang200027@gmail.com>
2021-06-13 15:02:44 -0400

    sum root to leaf with space O(d)

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -15,11 +15,26 @@ def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
 def preorder(tree, r, path):
     if not tree:
         return
-    newPath = path + [str(tree.data)]
+    path.append(str(tree.data))
     if not tree.left and not tree.right:
-        r.add("".join(newPath))
-    preorder(tree.left, r, newPath)
-    preorder(tree.right, r, newPath)
+        r.add("".join(path))
+    preorder(tree.left, r, path)
+    preorder(tree.right, r, path)
+    path.pop()
+
+# def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
+#     r = Result()
+#     preorder(tree, r, path=[])
+#     return r.total
+
+# def preorder(tree, r, path):
+#     if not tree:
+#         return
+#     newPath = path + [str(tree.data)]
+#     if not tree.left and not tree.right:
+#         r.add("".join(newPath))
+#     preorder(tree.left, r, newPath)
+#     preorder(tree.right, r, newPath)
 
 
 if __name__ == '__main__':
