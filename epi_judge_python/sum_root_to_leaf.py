cecdae3aa206ce674649e937979cf366c6e274b9
Darren <darrenzhang200027@gmail.com>
2021-06-13 15:00:05 -0400

    added sum root to leaf

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -1,10 +1,25 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+class Result:
+    def __init__(self):
+        self.total = 0
+    def add(self, nBin):
+        self.total += int(nBin, 2)
 
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
-    # TODO - you fill in here.
-    return 0
+    r = Result()
+    preorder(tree, r, path=[])
+    return r.total
+
+def preorder(tree, r, path):
+    if not tree:
+        return
+    newPath = path + [str(tree.data)]
+    if not tree.left and not tree.right:
+        r.add("".join(newPath))
+    preorder(tree.left, r, newPath)
+    preorder(tree.right, r, newPath)
 
 
 if __name__ == '__main__':
