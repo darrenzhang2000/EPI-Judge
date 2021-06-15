15f908c8a8f07fcf5b372f3d47e36d30c9208189
Darren <darrenzhang200027@gmail.com>
2021-06-14 20:45:35 -0400

    finished tree from preorder inorder

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -3,12 +3,69 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+class PreorderIdx:
+    def __init__(self):
+        self.idx = 0
 
 def binary_tree_from_preorder_inorder(preorder: List[int],
                                       inorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    if not preorder: 
+        return 
+    preorderIdx = PreorderIdx()
+    return r(preorder, inorder, preorderIdx, 0, len(preorder) - 1)
 
+def r(preorder, inorder, preorderIdx, left, right):
+    if left > right:
+        return
+
+    value = preorder[preorderIdx.idx]
+    preorderIdx.idx += 1
+
+    root = BinaryTreeNode(value)
+    rootIdx = inorder.index(value)
+    root.left = r(preorder, inorder, preorderIdx, left, rootIdx - 1)
+    root.right = r(preorder, inorder, preorderIdx, rootIdx + 1, right)
+    return root
+
+
+#     if not inorder:
+#         return 
+#     head = BinaryTreeNode(preorder[0])
+#     inIdx = 0
+#     appendLeft = True
+#     nodeToAppendTo = head
+#     ht = {preorder[0]: head}
+#     for i in range(1, len(preorder)):
+#         n = preorder[i]
+#         print(n)
+#         if n == inorder[inIdx]:
+#             nodeToAppendTo = insert(nodeToAppendTo, BinaryTreeNode(n), appendLeft)
+#             ht[n] = nodeToAppendTo
+#             while inIdx + 1 < len(inorder) and  inorder[inIdx + 1] in ht:
+#                 inIdx += 1
+#             nodeToAppendTo = ht[inorder[inIdx]]
+#             appendLeft = False
+#         else:
+#             nodeToAppendTo = insert(nodeToAppendTo, BinaryTreeNode(n), appendLeft)
+#             ht[n] = nodeToAppendTo
+#             appendLeft = True
+#     print('return', head)
+#     return head
+
+# def findNode(head, n):
+#     if head and head.data == n:
+#         return head
+#     if not head:
+#         return None
+#     return findNode(head.left, n) or findNode(head.right, n)
+
+
+# def insert(pre, cur, appendLeft):
+#     if appendLeft:
+#         pre.left = cur
+#     else:
+#         pre.right = cur
+#     return cur
 
 if __name__ == '__main__':
     exit(
