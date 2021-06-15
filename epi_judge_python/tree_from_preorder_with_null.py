a95222b99a6ad4850d41faf065ef9dc4f5c96f9c
Darren <darrenzhang200027@gmail.com>
2021-06-14 21:44:40 -0400

    finished tree from preorder with null

--- epi_judge_python/tree_from_preorder_with_null.py
+++ epi_judge_python/tree_from_preorder_with_null.py
@@ -5,10 +5,50 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
+class State:
+    def __init__(self):
+        self.state = 0
 
 def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    return r(iter(preorder))
+
+def r(preorderIt):
+    value = next(preorderIt)
+
+    if not value:
+        return
+
+    node = BinaryTreeNode(value)
+    node.left = r(preorderIt)
+    node.right = r(preorderIt)
+    return node
+
+# def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
+    # if not len(preorder):
+    #     return 0
+    # parentHt = {}
+    # head = nodeToAppendTo = BinaryTreeNode(preorder[0])
+    # s = State()
+    # for n in preorder:
+    #     if n == None:
+    #         s.state += 1
+    #         if s.state == 2:
+    #             # get parent with only 1 child
+    #             nodeToAppendTo = parentHt[nodeToAppendTo.data]
+    #             while nodeToAppendTo and parentHt[nodeToAppendTo.data].right != None:
+    #                 nodeToAppendTo = parentHt[nodeToAppendTo.data]
+    #             s.state = 0
+    #     elif s.state == 0:
+    #         nodeToAppendTo.left = BinaryTreeNode(n)
+    #         parentHt[n] = nodeToAppendTo
+    #         nodeToAppendTo = nodeToAppendTo.left
+    #     elif s.state == 1:
+    #         nodeToAppendTo.right = BinaryTreeNode(n)
+    #         parentHt[n] = nodeToAppendTo
+    #         nodeToAppendTo = nodeToAppendTo.right
+    #         s.state = 0
+    # print(head)
+    # return head
 
 
 @enable_executor_hook
