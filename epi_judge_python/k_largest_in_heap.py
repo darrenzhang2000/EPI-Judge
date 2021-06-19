1626bad5ce0b139a55c60b879f1e06a431633c87
Darren <darrenzhang200027@gmail.com>
2021-06-19 11:35:06 -0400

    added k largest in heap where k I can't add mutate original heap. time = O(klogk) space=O(k)

--- epi_judge_python/k_largest_in_heap.py
+++ epi_judge_python/k_largest_in_heap.py
@@ -1,11 +1,25 @@
 from typing import List
 
 from test_framework import generic_test, test_utils
-
+from heapq import heappush, heappop, heapify
 
 def k_largest_in_binary_heap(A: List[int], k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if k <= 0:
+        return
+    
+    candidates_max_heap = []
+    candidates_max_heap.append((-A[0], 0))
+    res = []
+    for _ in range(k):
+        idx = heappop(candidates_max_heap)[1]
+        res.append(A[idx])
+        # push children
+        if 2 * idx + 1 < len(A):
+            heappush(candidates_max_heap, (-A[2 * idx + 1], 2 * idx + 1 ))
+        if 2 * idx + 2 < len(A):
+            heappush(candidates_max_heap, (-A[2 * idx + 2], 2 * idx + 2))
+    return res
+
 
 
 if __name__ == '__main__':
