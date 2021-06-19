fb0ef20dfdd24581c50b3ac0fbc6669d94991103
Darren <darrenzhang200027@gmail.com>
2021-06-19 13:00:29 -0400

    finished int sqrt

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    l, r = 0, k
+    while l <= r:
+        m = l + (r - l) // 2
+        if m*m == k or m*m < k and (m+1)*(m+1) > k:
+            return m
+        elif m*m<k:
+            l = m + 1
+        else:
+            r = m - 1
 
 
 if __name__ == '__main__':
