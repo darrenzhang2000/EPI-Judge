fa8d1f54bbfabed921528ca70fdf8f0ce0d01956
Darren <darrenzhang200027@gmail.com>
2021-07-20 03:38:03 -0400

    added permutations

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -3,10 +3,31 @@ from typing import List
 from test_framework import generic_test, test_utils
 
 
-def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+# def permutations(A: List[int]) -> List[List[int]]:
+#     res = []
+#     def helper(a, s):
+#         if not s:
+#             res.append(a[:])
+#             return
+#         for v in s:
+#             a.append(v)
+#             helper(a[:], s-{v})   
+#     helper([], set(A))
+#     return res
 
+def permutations(A: List[int]) -> List[List[int]]:
+    def helper(nums):
+        if not nums: return [[]]
+        
+        ans = []
+
+        for num in nums:
+            for suffix in helper(nums - {num}):
+                ans.append([num]+suffix)
+
+        return ans
+
+    return helper(set(A))
 
 if __name__ == '__main__':
     exit(
