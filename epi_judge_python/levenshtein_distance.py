c20406c42af16265c2fcd767eae863845523223f
Darren <darrenzhang200027@gmail.com>
2021-07-29 19:01:55 -0400

    finished numbre of score combs and lev dist

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def levenshtein_distance(A: str, B: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    if not A or not B:
+        return max(len(A), len(B))
+
+    memo = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+    for j in range(1, len(B) + 1):
+        memo[0][j] = memo[0][j - 1] + 1
+    for i in range(1, len(A) + 1):
+        memo[i][0] = memo[i - 1][0] + 1
+    
+    for i in range(1, len(memo)):
+        for j in range(1, len(memo[0])):
+            cA, cB = A[i - 1], B[j - 1]
+            if cA != cB:
+                memo[i][j] = min(memo[i-1][j], memo[i][j-1], memo[i-1][j-1]) + 1
+            else:
+                memo[i][j] = memo[i-1][j-1]
+    return memo[-1][-1] 
 
 
 if __name__ == '__main__':
--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -5,9 +5,53 @@ from test_framework import generic_test
 
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    '''
+    let k = final_score, n = len(individual_play_scores)
+    time: O(kn), space O(kn)
+    space can be improved to O(n) 
+    '''
+    individual_play_scores.sort()
+    memo = [[1] + [0 for _ in range(final_score)] for _ in range(len(individual_play_scores))]
+    for i in range(len(memo)):
+        n = individual_play_scores[i]
+        for j in range(1, len(memo[0])):                
+            memo[i][j] = memo[i][j - n] if j - n >= 0 else 0
+            memo[i][j] += memo[i - 1][j] if i - 1 >= 0 else 0
+    return memo[-1][-1]
 
+
+
+
+
+    #count = [0]
+    #def r(score, start):
+    #    if score == 0:
+    #        count[0] += 1
+    #    elif score < 0:
+    #        return
+    #    else:
+    #        for i in range(start, len(individual_play_scores)):
+    #            n = individual_play_scores[i]
+    #            r(score - n, i)
+    #r(final_score, 0)
+
+    #return count[0]
+
+    '''
+    scores = 2, 3, 7
+    final_score = 12
+
+    include 2 or not include 2
+    include 3 or not include 3
+    include 7 or not include 7
+
+    2 2 2 2 2 2 
+              7
+
+    3
+
+    7
+    '''
 
 if __name__ == '__main__':
     exit(
