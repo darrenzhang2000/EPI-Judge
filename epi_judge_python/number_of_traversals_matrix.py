70290d01590571f560069f798753416b8f279f1d
Darren <darrenzhang200027@gmail.com>
2021-07-30 00:23:49 -0400

    added number of traversals

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -1,10 +1,33 @@
 from test_framework import generic_test
-
-
+import math
+import functools
 def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    '''
+    n - 1 downs
+    m - 1 rights
+    (m + n - 2) choose (m - 1)
+    return math.factorial(n - 1 + m - 1) // (math.factorial(n - 1) * math.factorial(m - 1))
+    '''
 
+    '''
+    @functools.lru_cache
+    def count_ways(n, m):
+        if n <= 1 or m <= 1:
+            return 1
+        from_left = count_ways(n, m - 1)
+        from_up = count_ways(n - 1, m)
+        return from_left + from_up
+
+    return count_ways(n, m)
+    '''
+    memo = [[1 for _ in range(n)] for _ in range(m)]
+    for i in range(1, len(memo)):
+        for j in range(1, len(memo[0])):
+            memo[i][j] = memo[i - 1][j] + memo[i][j - 1]
+    return memo[-1][-1]
+
+
+    
 
 if __name__ == '__main__':
     exit(
