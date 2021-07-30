0ba5980064459e0165326324e38e1b7a19cf801a
Darren <darrenzhang200027@gmail.com>
2021-07-30 00:35:35 -0400

    finished binomial coefficient

--- epi_judge_python/binomial_coefficients.py
+++ epi_judge_python/binomial_coefficients.py
@@ -1,10 +1,21 @@
 from test_framework import generic_test
+import functools
 
-
+@functools.lru_cache
 def compute_binomial_coefficient(n: int, k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    '''
+    nCk = 
 
+    5 balls, choose 3 red ball
+    4 balls, choose red + 4 balls, don't choose red
+    '''
+    if n == k or k == 0:
+        return 1
+
+    not_pick = compute_binomial_coefficient(n - 1, k)
+    pick = compute_binomial_coefficient(n - 1, k - 1)
+
+    return not_pick + pick
 
 if __name__ == '__main__':
     exit(
