5a566d4d3bdc28dd43c02d9654bd6d20b7191833
Darren <darrenzhang200027@gmail.com>
2021-07-31 18:25:36 -0400

    finished is string decomposable into words

--- epi_judge_python/is_string_decomposable_into_words.py
+++ epi_judge_python/is_string_decomposable_into_words.py
@@ -8,9 +8,51 @@ from test_framework.test_utils import enable_executor_hook
 
 def decompose_into_dictionary_words(domain: str,
                                     dictionary: Set[str]) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    last_length = [-1] * len(domain)
+    for i in range(len(domain)):
+        if domain[:i + 1] in dictionary:
+            last_length[i] = i + 1
+            continue
 
+        for j in range(i):
+            if last_length[j] != -1 and domain[j + 1:i + 1] in dictionary:
+                last_length[i] = i - j
+                break
+
+    decompositions = []
+    if last_length[-1] != -1:
+        idx = len(domain) - 1
+        while idx >= 0:
+            decompositions.append(domain[idx + 1 - last_length[idx]:idx + 1])
+            idx -= last_length[idx]
+        decompositions = decompositions[::-1]
+    return decompositions
+
+
+    # res = []
+    # found = [False]
+    # def r(domain, res):
+    #     if found[0]:
+    #         return
+
+    #     for i in range(len(domain)):
+    #         prefix = domain[:i + 1]            
+    #         if prefix in dictionary: 
+    #             res.append(prefix)
+    #             suffix = domain[i + 1:]
+    #             print(res, prefix, suffix)
+
+    #             if suffix == "":
+    #                 found[0] = True
+    #                 return
+        
+    #             r(suffix, res)
+
+    #             res.pop()
+            
+    # r(domain, res)
+    # print(res)
+    # return res
 
 @enable_executor_hook
 def decompose_into_dictionary_words_wrapper(executor, domain, dictionary,
