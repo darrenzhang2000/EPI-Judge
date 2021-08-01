c836e7497aef58acd5aca5bde9f57c0f8de2181f
Darren <darrenzhang200027@gmail.com>
2021-07-31 20:39:49 -0400

    finished add number of traversals staircase

--- epi_judge_python/number_of_traversals_staircase.py
+++ epi_judge_python/number_of_traversals_staircase.py
@@ -1,10 +1,27 @@
 from test_framework import generic_test
-
+import functools
 
 def number_of_ways_to_top(top: int, maximum_step: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    '''
+    n = 5, k = 3
 
+    r(5) = r(4) + r(3) + r(2)
+    r(4) = r(3) + r(2) + r(1)
+    r(3) = r(2) + (1) + r(0)
+
+    '''
+    @functools.lru_cache
+    def r(n, k):
+        if n < 0:
+            return 0
+        if n <= 1:
+            return 1
+        total = 0
+        for i in range(1, maximum_step + 1):
+            total += r(n - i, k)
+        return total
+    
+    return r(top, maximum_step)
 
 if __name__ == '__main__':
     exit(
