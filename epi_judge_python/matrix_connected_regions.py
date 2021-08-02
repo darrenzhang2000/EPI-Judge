d70cd7657d3226dd9bbe1c9771a178ec7255a5d0
Darren <darrenzhang200027@gmail.com>
2021-08-02 18:47:17 -0400

    finished matrix connected regions

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -4,9 +4,19 @@ from test_framework import generic_test
 
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
-    return
+    color = image[x][y]
+    flipped_color = 1 - color
+    def flip_if_color(x, y):
+        if not (0 <= x < len(image) and 0 <= y < len(image[0])) or image[x][y] != color:
+            return
+        
+        image[x][y] = flipped_color
+        offsets = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for dx, dy in offsets:
+            flip_if_color(x + dx, y + dy)
 
+    flip_if_color(x, y)
+    return image    
 
 def flip_color_wrapper(x, y, image):
     flip_color(x, y, image)
