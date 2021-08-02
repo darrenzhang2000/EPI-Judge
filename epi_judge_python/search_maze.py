b377a4726245d0af4cc21bd3d3fa3a568b299a3e
Darren <darrenzhang200027@gmail.com>
2021-08-02 17:15:30 -0400

    epi search maze soln

--- epi_judge_python/search_maze.py
+++ epi_judge_python/search_maze.py
@@ -14,8 +14,48 @@ Coordinate = collections.namedtuple('Coordinate', ('x', 'y'))
 
 def search_maze(maze: List[List[int]], s: Coordinate,
                 e: Coordinate) -> List[Coordinate]:
-    # TODO - you fill in here.
-    return []
+    def dfs(cur):
+        if not (0 <= cur.x < len(maze) and 0 <= cur.y < len(maze[cur.x]) and maze[cur.x][cur.y] == WHITE):
+            return False
+        path.append(cur)
+        maze[cur.x][cur.y] = BLACK
+        if cur == e:
+            return True
+        
+        if any(
+            map(
+                dfs,
+                map(Coordinate, (cur.x - 1, cur.x + 1, cur.x, cur.x), (cur.y, cur.y, cur.y - 1, cur.y + 1))
+        )):
+            return True
+
+        del path[-1]
+        return False
+    
+    path = []
+    dfs(s)
+    return path
+    # path = []
+    # visited = set()
+    # found = [False]
+    # def dfs(x, y):
+    #     print(found)
+    #     if (x, y) in visited or found[0]:
+    #         return
+    #     visited.add((x, y))
+    #     path.append(Coordinate(x, y))
+    #     if x == e.x and y == e.y:
+    #         found[0] = True
+    #         return
+    #     offsets = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    #     for offset in offsets:
+    #         new_x, new_y = x + offset[0], y + offset[1]
+    #         if 0 <= new_x < len(maze) and 0 <= new_y < len(maze[0]):
+    #             dfs(new_x, new_y)
+    #     path.pop()
+    #     visited.remove((x, y))
+    # dfs(s.x, s.y)
+    # return len(path) > 0
 
 
 def path_element_is_feasible(maze, prev, cur):
