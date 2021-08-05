8ccf6f9a0e0e5bc2ee9fe3efd96d3ddf02a9e24b
Darren <darrenzhang200027@gmail.com>
2021-08-05 16:41:59 -0400

    finished string transformability

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,11 +1,42 @@
 from typing import Set
-
+import collections
+import string
 from test_framework import generic_test
 
 
 def transform_string(D: Set[str], s: str, t: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    '''
+    brute force:
+    go through the s string,
+        go thru all letters of alphabet
+            check if new string exists in set, then call recursive algorithm
+    O(s!)
+
+    optimized soln:
+    build a undirected graph where each edge seperates two vertices with exactly one letter difference
+    check if it's possible to reach vertex s to vertex t
+    O(v+e) 
+    '''
+    if len(s) != len(t):
+        return False
+
+    StringWithDistance = collections.namedtuple('StringWithDistance', ('candidate_string', 'distance'))
+    q = collections.deque([StringWithDistance(s, 0)])
+    D.remove(s)
+
+    while q:
+        f = q.popleft()
+        if f.candidate_string == t:
+            return f.distance
+        for i in range(len(f.candidate_string)):
+            for c in string.ascii_lowercase:
+                cand = f.candidate_string[:i] + c + f.candidate_string[i + 1:]
+                if cand in D:
+                    D.remove(cand)
+                    q.append(StringWithDistance(cand, f.distance + 1))
+    return -1
+    
+
 
 
 if __name__ == '__main__':
